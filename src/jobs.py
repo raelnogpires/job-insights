330c3d0de275dab372807e251693e35e6446994b
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-08-04 16:04:20 -0300

    bug: should be correct, cant find the error

--- src/jobs.py
+++ src/jobs.py
@@ -1,7 +1,11 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    # code
-    return []
+    with open(path, encoding="utf-8") as file:
+        csv_reader = csv.reader(file, delimiter=",", quotechar='"')
+        header, *data = csv_reader
+
+    return [header, data]
