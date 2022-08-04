b8a1762b06c08af22d8cca71140961f2cee2e2a2
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-08-04 16:25:56 -0300

    feat: finish implementation of read func

--- src/jobs.py
+++ src/jobs.py
@@ -5,7 +5,9 @@ import csv
 @lru_cache
 def read(path):
     with open(path, encoding="utf-8") as file:
-        csv_reader = csv.reader(file, delimiter=",", quotechar='"')
-        header, *data = csv_reader
+        result = []
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            result.append(row)
 
-    return [header, data]
+        return result
