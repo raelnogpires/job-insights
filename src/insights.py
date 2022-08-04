6c0a2c32901939ddfc6fa889c3063e9e3c38af49
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-08-04 16:57:55 -0300

    feat: complete implementation of get_unique_industries func

--- src/insights.py
+++ src/insights.py
@@ -5,7 +5,8 @@ def get_unique_job_types(path):
     result = set()
     jobs = read(path)
     for job in jobs:
-        result.add(job["job_type"])
+        if job["job_type"] != "":
+            result.add(job["job_type"])
 
     return result
 
@@ -29,21 +30,13 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    result = set()
+    jobs = read(path)
+    for job in jobs:
+        if job["industry"] != "":
+            result.add(job["industry"])
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return result
 
 
 def filter_by_industry(jobs, industry):
