073407a75ffa6a9b83c9afb8a947ebbc14133c98
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-08-05 13:54:37 -0300

    test: implement test of read_brazilian_file func

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+
+path = "tests/mocks/brazilians_jobs.csv"
 
 
 def test_brazilian_jobs():
-    pass
+    jobs = read_brazilian_file(path)
+    for job in jobs:
+        assert "title" in job
+        assert "salary" in job
+        assert "type" in job
