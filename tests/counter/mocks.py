bb6a9c93ba149e2600e00bd62bca7afd5b54cb45
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-08-10 13:33:58 -0300

    still removing some files

--- tests/counter/mocks.py
+++
@@ -1,8 +0,0 @@
-def count_word_ocurrences(path, word):
-    file = open(path, "r")
-    read_data = file.read()
-    word_count = read_data.count(word)
-    return word_count
-
-
-print(count_word_ocurrences("src/jobs.csv", "Python"))
